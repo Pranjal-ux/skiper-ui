3e1400c033753e6bcc2743ebd9cb60be5daaf920
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-10 20:12:07 +0530

    #CardCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,36 @@
 "use client";
 import React from "react";
-import TextScroll from "../components/ui/text-scroll";
+import CardCarousel from "../components/ui/card-carousel";
+// import TextScroll from "../components/ui/text-scroll";
 // import FlipLink from '../components/ui/text-effect-flipper'
 // import WrapButton from "@/components/ui/wrap-button"
 
 // import{Banana,Twitter,Instagram,Github} from "lucide-react";
 // import ShareButton from "@/components/ui/share-button"
 const page = () => {
+  const images = [
+    {
+      src: "/img1.jpg",
+      alt: "Image 1",
+    },
+    {
+      src: "/img2.jpg",
+      alt: "Image 2",
+    },
+    {
+      src: "/img3.jpg",
+      alt: "Image 3",
+    },
+  ];
   return (
     <div className=" ">
-      <div className="h-screen"></div>
+      <CardCarousel
+        autoplayDelay={2000}
+        showPagination={false}
+        showNavigation={true}
+        images={images}
+      />
+      {/* <div className="h-screen"></div>
       <div className="h-screen border-t-2 ">
         <TextScroll
           className="text-4xl"
@@ -17,7 +38,7 @@ const page = () => {
           text="Wlecome to New york city!!"
         ></TextScroll>
       </div>
-      <div className="h-screen border-t-2"></div>
+      <div className="h-screen border-t-2"></div> */}
 
       {/* <FlipLink href="https://x.com/guri_who">Behance</FlipLink>
 <FlipLink href="https://github.com/Pranjal-ux">GITHUB</FlipLink>
