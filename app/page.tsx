1ed3ee5723b800ee8f7aea7e8592ad5309914c8c
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-10 19:49:04 +0530

    #TextScroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,38 @@
-"use client"
-import React from 'react'
- import FlipLink from '../components/ui/text-effect-flipper'
+"use client";
+import React from "react";
+import TextScroll from "../components/ui/text-scroll";
+// import FlipLink from '../components/ui/text-effect-flipper'
 // import WrapButton from "@/components/ui/wrap-button"
 
 // import{Banana,Twitter,Instagram,Github} from "lucide-react";
 // import ShareButton from "@/components/ui/share-button"
 const page = () => {
   return (
-   <div className="flex justify-center items-center flex-col">
+    <div className=" ">
+      <div className="h-screen"></div>
+      <div className="h-screen border-t-2 ">
+        <TextScroll
+          className="text-4xl"
+          default_velocity={-5}
+          text="Wlecome to New york city!!"
+        ></TextScroll>
+      </div>
+      <div className="h-screen border-t-2"></div>
 
-
-
-
-
-
-
-<FlipLink href="https://x.com/guri_who">Behance</FlipLink>
+      {/* <FlipLink href="https://x.com/guri_who">Behance</FlipLink>
 <FlipLink href="https://github.com/Pranjal-ux">GITHUB</FlipLink>
 <FlipLink href="https://x.com/guri_who">Twitter</FlipLink>
 <FlipLink href="https://x.com/guri_who">Instagram</FlipLink>
-<FlipLink href="https://x.com/guri_who">Facebook</FlipLink>
+<FlipLink href="https://x.com/guri_who">Facebook</FlipLink> */}
 
-
-
-
-
-
-
-
-
-
-  {/* /* <h1 className="text-white">Page</h1>
+      {/* /* <h1 className="text-white">Page</h1>
     <WrapButton>
        
       Get Started
     </WrapButton>
-    <ShareButton links={[{icon:Twitter},{icon:Banana},{icon:Instagram},{icon:Github}]}>Share</ShareButton> */ }
-   </div>
+    <ShareButton links={[{icon:Twitter},{icon:Banana},{icon:Instagram},{icon:Github}]}>Share</ShareButton> */}
+    </div>
+  );
+};
 
-
-      
-    
-
-
- 
-  )
-}
-
-export default page
+export default page;
